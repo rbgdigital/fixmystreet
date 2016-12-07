3e601694fedb5e0b64b6865f9fb741dd0d46e8e4
Dave Arter <davea@mysociety.org>
2016-12-07 12:05:31 +0000

    Use real values from reports when generating Exor RDI

--- perllib/FixMyStreet/App/Controller/Admin/ExorDefects.pm
+++ perllib/FixMyStreet/App/Controller/Admin/ExorDefects.pm
@@ -3,6 +3,8 @@ use Moose;
 use namespace::autoclean;
 
 use Text::CSV;
+use DateTime;
+use mySociety::Random qw(random_bytes);
 
 BEGIN { extends 'Catalyst::Controller'; }
 
@@ -18,20 +20,21 @@ sub index : Path : Args(0) {
 
     my $csv = Text::CSV->new({ binary => 1, eol => "" });
 
-    my ($p_count, $i_count, $j_count, $m_count) = (0, 0, 0, 0);
+    my $p_count = 0;
 
     # RDI first line is always the same
     $csv->combine("1", "1.8", "1.0.0.0", "ENHN", "");
     my @body = ($csv->string);
 
     # Let's just group all defects into a single inspection/sequence for now
+    my $now = DateTime->now( time_zone => FixMyStreet->time_zone || FixMyStreet->local_time_zone );
     $csv->combine(
         "G", # start of an area/sequence
-        "35164", # randomised sequence number
+        int(rand(99999)), # randomised sequence number
         "","", # must be empty
         "M T", # inspector initials
-        "161111", # date of inspection yymmdd
-        "1459", # time of inspection hhmm
+        $now->strftime("%y%m%d"), # date of inspection yymmdd
+        $now->strftime("%H%M"), # time of inspection hhmm
         "D", # inspection variant, should always be D
         "INS", # inspection type, always INS
         "N", # Area of the county - north (N) or south (S)
@@ -53,27 +56,26 @@ sub index : Path : Args(0) {
             "I", # beginning of defect record
             "MC", # activity code - minor carriageway, also FC (footway)
             "", # empty field, can also be A (seen on MC) or B (seen on FC)
-            "46", # randomised sequence number
-            "betw 21-23 cotswold cres-chipping norton", # defect location field
-            "1459", # defect time raised
+            $report->id, # randomised sequence number
+            "", # defect location field, which we don't capture from inspectors
+            $report->lastupdate->strftime("%H%M"), # defect time raised
             "","","","","","","", # empty fields
-            "m.t -TM none, fill in 300 x 300 x 50 (2 of 3)", # defect description
-            "pothole in white ", # further description
+            $report->get_extra_metadata('detailed_information'), # defect description
+            $report->get_extra_metadata('traffic_information'), # further description
         );
         push @body, $csv->string;
-        $i_count++;
 
+        my ($eastings, $northings) = $report->local_coords;
         $csv->combine(
             "J", # georeferencing record
-            "POT2", # defect type - SFP2: sweep and fill <1m2, POT2 also seen
-            "2", # priority of defect
+            $report->get_extra_metadata('size'), # defect type - SFP2: sweep and fill <1m2, POT2 also seen
+            $report->get_extra_metadata('priority'), # priority of defect
             "","", # empty fields
-            "431448.472269547", # eastings
-            "226370.94024575", # northings
+            $eastings, # eastings
+            $northings, # northings
             "","","","","" # empty fields
         );
         push @body, $csv->string;
-        $j_count++;
 
         $csv->combine(
             "M", # bill of quantities record
@@ -83,30 +85,30 @@ sub index : Path : Args(0) {
             "", "" # empty fields
         );
         push @body, $csv->string;
-        $m_count++;
     }
 
     # end this group of defects with a P record
     $csv->combine(
         "P", # end of area/sequence
-        "0", # always 0
-        "999999", # charging code, always 999999 in OCC
+        0, # always 0
+        999999, # charging code, always 999999 in OCC
     );
     push @body, $csv->string;
     $p_count++;
 
     # end the RDI file with an X record
+    my $record_count = $problems->count;
     $csv->combine(
         "X", # end of inspection record
-        "$p_count",
-        "$p_count",
-        "$i_count",
-        "$j_count",
-        "0", "0", "0", # always zero
-        "$m_count",
-        "0", # always zero
-        "$p_count",
-        "0", "0", "0" # error counts, always zero
+        $p_count,
+        $p_count,
+        $record_count, # number of I records
+        $record_count, # number of J records
+        0, 0, 0, # always zero
+        $record_count, # number of M records
+        0, # always zero
+        $p_count,
+        0, 0, 0 # error counts, always zero
     );
     push @body, $csv->string;
 
