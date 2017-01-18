25b8cd8b648dbcf91bc56b613afb5ee45d715caa
pezholio <pezholio@gmail.com>
2017-01-18 16:24:45 +0000

    Redirect via auth with params

--- perllib/FixMyStreet/App/Controller/Auth.pm
+++ perllib/FixMyStreet/App/Controller/Auth.pm
@@ -222,7 +222,7 @@ sub token : Path('/M') : Args(1) {
     $c->authenticate( { email => $user->email }, 'no_password' );
 
     # send the user to their page
-    $c->detach( 'redirect_on_signin', [ $data->{r} ] );
+    $c->detach( 'redirect_on_signin', [ $data->{r}, $data->{p} ] );
 }
 
 =head2 facebook_sign_in
@@ -410,13 +410,17 @@ Used after signing in to take the person back to where they were.
 
 
 sub redirect_on_signin : Private {
-    my ( $self, $c, $redirect ) = @_;
+    my ( $self, $c, $redirect, $params ) = @_;
     $redirect = 'my' unless $redirect;
     $redirect = 'my' if $redirect =~ /^admin/ && !$c->user->is_superuser;
     if ( $c->cobrand->moniker eq 'zurich' ) {
         $redirect = 'admin' if $c->user->from_body;
     }
-    $c->res->redirect( $c->uri_for( "/$redirect" ) );
+    if (defined $params) {
+        $c->res->redirect( $c->uri_for( "/$redirect", $params ) );
+    } else {
+        $c->res->redirect( $c->uri_for( "/$redirect" ) );
+    }
 }
 
 =head2 redirect
