18dba2d98113925def3654ece52100f0fdae409c
Dave Arter <davea@mysociety.org>
2017-01-26 15:12:25 +0000

    Group inspections by inspector within RDI file

--- perllib/FixMyStreet/App/Controller/Admin/ExorDefects.pm
+++ perllib/FixMyStreet/App/Controller/Admin/ExorDefects.pm
@@ -63,13 +63,11 @@ sub download : Path('download') : Args(0) {
         ]
     );
 
-    my $initials = "XX";
     my $user;
     if ( $c->get_param('user_id') ) {
         my $uid = $c->get_param('user_id');
         $params{'admin_log_entries.user_id'} = $uid;
         $user = $c->model('DB::User')->find( { id => $uid } );
-        $initials = $user->initials if $user;
     }
 
     my $problems = $c->cobrand->problems->search(
@@ -92,6 +90,14 @@ sub download : Path('download') : Args(0) {
         $c->res->redirect( $c->uri_for( '' ) );
     }
 
+    # A single RDI file might contain inspections from multiple inspectors, so
+    # we need to group inspections by inspector within G records.
+    my $inspectors = {};
+    while ( my $report = $problems->next ) {
+        my $user_id = $report->inspection_log_entry->user->id;
+        $inspectors->{$user_id} ||= [];
+        push @{ $inspectors->{$user_id} }, $report;
+    }
 
     my $csv = Text::CSV->new({ binary => 1, eol => "" });
 
@@ -102,78 +108,81 @@ sub download : Path('download') : Args(0) {
     $csv->combine("1", "1.8", "1.0.0.0", "ENHN", "");
     my @body = ($csv->string);
 
-    # Let's just group all defects into a single inspection/sequence for now
-    $csv->combine(
-        "G", # start of an area/sequence
-        $link_id, # area/link id, fixed value for our purposes
-        "","", # must be empty
-        $initials, # inspector initials
-        $start_date->strftime("%y%m%d"), # date of inspection yymmdd
-        "0700", # time of inspection hhmm, set to static value for now
-        "D", # inspection variant, should always be D
-        "INS", # inspection type, always INS
-        "N", # Area of the county - north (N) or south (S)
-        "", "", "", "" # empty fields
-    );
-    push @body, $csv->string;
+    my $i = 0;
+    foreach my $inspections (values %$inspectors) {
+        my $initials = @$inspections[0]->inspection_log_entry->user->get_extra_metadata('initials');
 
-    $csv->combine(
-        "H", # initial inspection type
-        "MC" # minor carriageway (changes depending on activity code)
-    );
-    push @body, $csv->string;
-
-    my $i = 1;
-    while ( my $report = $problems->next ) {
-        my ($eastings, $northings) = $report->local_coords;
-        my $description = sprintf("%s %s", $report->external_id || "", $report->get_extra_metadata('detailed_information') || "");
         $csv->combine(
-            "I", # beginning of defect record
-            "MC", # activity code - minor carriageway, also FC (footway)
-            "", # empty field, can also be A (seen on MC) or B (seen on FC)
-            sprintf("%03d", $i++), # randomised sequence number
-            "${eastings}E ${northings}N", # defect location field, which we don't capture from inspectors
-            $report->inspection_log_entry->whenedited->strftime("%H%M"), # defect time raised
-            "","","","","","","", # empty fields
-            $report->get_extra_metadata('traffic_information') ? 'TM required' : 'TM none', # further description
-            $description, # defect description
+            "G", # start of an area/sequence
+            $link_id, # area/link id, fixed value for our purposes
+            "","", # must be empty
+            $initials || "XX", # inspector initials
+            $start_date->strftime("%y%m%d"), # date of inspection yymmdd
+            "0700", # time of inspection hhmm, set to static value for now
+            "D", # inspection variant, should always be D
+            "INS", # inspection type, always INS
+            "N", # Area of the county - north (N) or south (S)
+            "", "", "", "" # empty fields
         );
         push @body, $csv->string;
 
         $csv->combine(
-            "J", # georeferencing record
-            $report->get_extra_metadata('defect_type') || 'SFP2', # defect type - SFP2: sweep and fill <1m2, POT2 also seen
-            $report->response_priority ?
-                $report->response_priority->external_id :
-                "2", # priority of defect
-            "","", # empty fields
-            $eastings, # eastings
-            $northings, # northings
-            "","","","","" # empty fields
+            "H", # initial inspection type
+            "MC" # minor carriageway (changes depending on activity code)
         );
         push @body, $csv->string;
 
+        foreach my $report (@$inspections) {
+            my ($eastings, $northings) = $report->local_coords;
+            my $description = sprintf("%s %s", $report->external_id || "", $report->get_extra_metadata('detailed_information') || "");
+            $csv->combine(
+                "I", # beginning of defect record
+                "MC", # activity code - minor carriageway, also FC (footway)
+                "", # empty field, can also be A (seen on MC) or B (seen on FC)
+                sprintf("%03d", ++$i), # randomised sequence number
+                "${eastings}E ${northings}N", # defect location field, which we don't capture from inspectors
+                $report->inspection_log_entry->whenedited->strftime("%H%M"), # defect time raised
+                "","","","","","","", # empty fields
+                $report->get_extra_metadata('traffic_information') ? 'TM required' : 'TM none', # further description
+                $description, # defect description
+            );
+            push @body, $csv->string;
+
+            $csv->combine(
+                "J", # georeferencing record
+                $report->get_extra_metadata('defect_type') || 'SFP2', # defect type - SFP2: sweep and fill <1m2, POT2 also seen
+                $report->response_priority ?
+                    $report->response_priority->external_id :
+                    "2", # priority of defect
+                "","", # empty fields
+                $eastings, # eastings
+                $northings, # northings
+                "","","","","" # empty fields
+            );
+            push @body, $csv->string;
+
+            $csv->combine(
+                "M", # bill of quantities record
+                "resolve", # permanent repair
+                "","", # empty fields
+                "/CMC", # /C + activity code
+                "", "" # empty fields
+            );
+            push @body, $csv->string;
+        }
+
+        # end this group of defects with a P record
         $csv->combine(
-            "M", # bill of quantities record
-            "resolve", # permanent repair
-            "","", # empty fields
-            "/CMC", # /C + activity code
-            "", "" # empty fields
+            "P", # end of area/sequence
+            0, # always 0
+            999999, # charging code, always 999999 in OCC
         );
         push @body, $csv->string;
+        $p_count++;
     }
 
-    # end this group of defects with a P record
-    $csv->combine(
-        "P", # end of area/sequence
-        0, # always 0
-        999999, # charging code, always 999999 in OCC
-    );
-    push @body, $csv->string;
-    $p_count++;
-
     # end the RDI file with an X record
-    my $record_count = $problems->count;
+    my $record_count = $i;
     $csv->combine(
         "X", # end of inspection record
         $p_count,
@@ -190,7 +199,11 @@ sub download : Path('download') : Args(0) {
 
     my $start = $start_date->strftime("%Y%m%d");
     my $end = $end_date->strftime("%Y%m%d");
-    my $filename = sprintf("exor_defects-%s-%s-%s.rdi", $start, $end, $initials);
+    my $filename = sprintf("exor_defects-%s-%s.rdi", $start, $end);
+    if ( $user ) {
+        my $initials = $user->get_extra_metadata("initials") || "";
+        $filename = sprintf("exor_defects-%s-%s-%s.rdi", $start, $end, $initials);
+    }
     $c->res->content_type('text/csv; charset=utf-8');
     $c->res->header('content-disposition' => "attachment; filename=$filename");
     # The RDI format is very weird CSV - each line must be wrapped in
