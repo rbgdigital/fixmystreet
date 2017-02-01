676e6fb963a39a94d31db65a462f9b2914a39820
Dave Arter <davea@mysociety.org>
2017-02-01 11:40:38 +0000

    Allow cobrands to control which states affect reputation
    Rather than assigning +1 to a user's reputation every time a report is
    inspected, this allows cobrands to specify which states should increment or
    decrement the reputation value when a report is inspected.
    
    The default behaviour is for reputation to never be changed, but the Oxfordshire
    cobrand will increment the reputation when a report is marked as 'action
    scheduled'.
    
    For mysociety/fixmystreetforcouncils#119

--- perllib/FixMyStreet/Cobrand/Default.pm
+++ perllib/FixMyStreet/Cobrand/Default.pm
@@ -1187,4 +1187,18 @@ sub category_extra_hidden {
 	return 0;
 }
 
+=head2 reputation_increment_states/reputation_decrement_states
+
+Get a list of states that cause the reporting user's reputation to be
+incremented/decremented, if a report is changed to this state upon inspection.
+
+If called in array context then returns an array of names, otherwise returns a
+HASHREF.
+
+=cut
+
+sub reputation_increment_states { return wantarray ? () : {} };
+sub reputation_decrement_states { return wantarray ? () : {} };
+
+
 1;
